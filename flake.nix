29a4ebae8cf035890b7671736d1f883de66e6571
louisgundelwein <l.gundelwein@proton.me>
2025-03-26 18:26:00 +0100

    fix: flake.nix

--- flake.nix
+++ flake.nix
@@ -1,19 +1,16 @@
 {
-  description = "My custom nixos config";
+  description = "Louis' modular NixOS config";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      {
-        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./hosts/nixos.nix
-          ];
-        };
-      });
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./hosts/nixos.nix
+      ];
+    };
+  };
 }
