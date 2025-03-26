964ee63846f2e3d84a37124c68ecaacbfe63b60b
louisgundelwein <l.gundelwein@proton.me>
2025-03-26 18:32:38 +0100

    fixes

--- hosts/nixos.nix
+++ hosts/nixos.nix
@@ -8,7 +8,13 @@
     ../modules/users.nix
   ];
 
+  # Basic system configuration
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   networking.hostName = "nixos";
   nixpkgs.config.allowUnfree = true;
-  system.stateVersion = "24.11";
+  
+  # This value determines the NixOS release with which your system is to be compatible
+  system.stateVersion = "24.11"; # Did you read the comment?
 }
